dd9ed1aacc31b766189a70fa83ae678ccbf6f49e
codeeve <rpgmania37@gmail.com>
2026-03-09 10:45:35 +0800

    ini di commit dari terminal

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Welcome to Java programming.");
+
+        System.out.println("Java is a versatile and powerful programming language.");
     }
 }
